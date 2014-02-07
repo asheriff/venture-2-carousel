b418dea1d1b8aa92c27b1751126048f72269b279
Aaron Sheriff <aaron.sheriff@gmail.com>
2014-02-06 22:30:31 -0600

    Add css3 transition, modernizer detection

--- dist/js/site.js
+++ dist/js/site.js
@@ -5,6 +5,7 @@ $(function(){
   var $image_carousel = $('#ImageCarousel');
   var $content_carousel = $('#ContentCarousel');
 
+
   // Function to set the current tab, as well as the various carousels.
   var setCurrentTab = function(idx) {
     // Bail out if UI already doing it's thang.
@@ -27,20 +28,32 @@ $(function(){
     });
   };
 
+
+  // Define the css3 transition, or fallback to jquery animations.
+  var transitions = Modernizr.csstransitions && {
+    transforms: Modernizr.csstransforms,
+    transforms3d: Modernizr.csstransforms3d,
+    easing: 'ease'
+  };
+
   // Create the jcarousel objects.
   $image_carousel.jcarousel({
-    list: '.slides'
+    list: '.slides',
+    transitions: transitions
   });
 
   $content_carousel.jcarousel({
-    list: '.slides'
+    list: '.slides',
+    transitions: transitions
   });
+
 
   // Hook-up click handler on the tabs.
   $tabs.on('click', function(event){
     event.preventDefault();
     setCurrentTab($(this).index());
   });
+
 
   // Initialize the 1st tab as active.
   setCurrentTab(0);
